a1bcaa05263ca3aa311620fee151de5caeabd4a8
RAMAVATH SAI CHARAN <149686834+saicharan1853@users.noreply.github.com>
2026-05-22 14:32:12 +0530

    Update main.ts

--- expense-tracker-backend/src/main.ts
+++ expense-tracker-backend/src/main.ts
@@ -1,14 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://expense-tracker-dun-six-19.vercel.app',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'https://expense-tracker-dun-six-19.vercel.app',
     credentials: true,
   });
 
