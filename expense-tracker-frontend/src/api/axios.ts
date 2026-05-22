651e9baa5c26f61b78492ea7a0fcca77cd121744
RAMAVATH SAI CHARAN <149686834+saicharan1853@users.noreply.github.com>
2026-05-22 13:16:23 +0530

    Update axios.ts

--- expense-tracker-frontend/src/api/axios.ts
+++ expense-tracker-frontend/src/api/axios.ts
@@ -3,14 +3,16 @@ import axios from 'axios'
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-'https://expense-tracker-backend.onrender.com';
+    'https://expense-tracker-backend.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
+
   return config
 })
 
